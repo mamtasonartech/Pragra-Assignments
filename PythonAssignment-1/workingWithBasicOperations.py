93547141e419f5cb2100cc6bba07652e45eba98c
mamtasonartech <sonarprasadtech@gmail.com>
2025-12-11 08:03:40 -0500

    Working With Basic Operations.

---
+++ PythonAssignment-1/workingWithBasicOperations.py
@@ -0,0 +1,29 @@
+#Working With Basic Operations
+
+# 6.	Create two integer variables and print their multiplication.
+num1 = 10
+num2 = 2
+mul = num1 * num2
+print(mul)
+
+# 7.	Create a float variable and add 10 to it. Print the result.
+fnum1 = 5.25
+print(fnum1 + 10)
+
+# 8.	Create two variables containing numbers. Print which one is greater.
+number3 = 10
+number4 = 20
+if number3 > number4:
+        print("Number 3 is greater than Number 4")
+else:
+        print("Number 4 is greater than Number 3")
+
+# 9.	Create a variable that stores a number. Print the number plus 5.
+number5 = 20
+print(number5 + 5)
+
+# 10.	Create two variables with numbers and print their difference.
+number6 = 25
+number7 = 20
+diff = number6 - number7
+print(diff)
