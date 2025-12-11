45520564f324fcad869a3c81b4c162a0052dc6bd
mamtasonartech <sonarprasadtech@gmail.com>
2025-12-11 10:25:10 -0500

    Worked on Theory questions and some changes.

--- PythonAssignment-1/stringVariables.py
+++ PythonAssignment-1/stringVariables.py
@@ -6,8 +6,7 @@ print(myIntro)
 
 # 12.	Store your school name in a variable and print it twice.
 schoolName= "BHS ONGC"
-print(schoolName)
-print(schoolName)
+print(schoolName , schoolName)
 
 # 13.	Create a variable to store your friend's name and print "Hello " followed by that variable.
 myFriend = "Sanu"
@@ -16,7 +15,8 @@ print(myFriend + " " + "Hello")
 # 14.	Create two string variables first_name and last_name, and print them together.
 first_name = "Mamta"
 last_name = "Sonar"
-print(first_name + " " + last_name)
+full_name = first_name + " " + last_name
+print(full_name)
 
 # 15.	Store a word in a variable and print its length using len() (this is allowed since it's not a string method).
 myName= "Mamta"
