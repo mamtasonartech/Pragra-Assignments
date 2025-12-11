650113f206a02ad61ea3a415757feff29a8c7cb1
mamtasonartech <sonarprasadtech@gmail.com>
2025-12-11 07:36:01 -0500

    1st section basic Variable Creation.

---
+++ PythonAssignment-1/BasicVariableCreation.py
@@ -0,0 +1,28 @@
+#Basic Variable Creation
+#1.	Create a variable to store your name and print it.
+myName = "Mamta"
+print("My name is : " + myName)
+
+#2.	Create two integer variables and print their sum.
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print(sum)
+
+#3.	Create three float variables and print their average.
+fnum1 = 2.25
+fnum2 = 3.5
+fnum3 = 5.5
+faverage= (fnum1+fnum2+fnum3)/3
+print(faverage)
+
+#4.	Store your age in a variable and print it.
+myAge = 35
+print(myAge)
+
+
+#5.	Create a variable that stores a boolean value (True or False) and print it.
+goodAtBadminton = False
+print(goodAtBadminton)
+loveCricket = True
+print(loveCricket)
