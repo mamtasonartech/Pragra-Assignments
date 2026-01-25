cc3434e1fc715c711e75ed54066e7b54e3fac18b
mamtasonartech <sonarprasadtech@gmail.com>
2026-01-24 19:25:10 -0500

    Assignment-7 : HARD LEVEL (21-25 Questions) Goal: Logic + multiple methods + object interaction

---
+++ PythonAssignment-7/hard_level.py
@@ -0,0 +1,264 @@
+"""21. Create a class BankAccount with methods:
+● deposit(amount)
+● withdraw(amount)
+● display_balance()
+Ensure withdrawal is allowed only if balance is sufficient."""
+
+# Create a class named BankAccount
+class BankAccount:
+# Initialize the account with a balance attribute (default 0)
+    def __init__(self,balance = 0):
+        self.balance = balance
+# Define a deposit method:
+#   - It should accept an amount
+#   - Add the amount to the balance
+    def deposit(self,amount):
+        self.balance += amount
+
+# Define a withdraw method:
+#   - It should accept an amount
+#   - First check if balance is sufficient
+#   - If yes, subtract amount from balance
+#   - If no, print "Insufficient balance"
+    def withdraw(self,amount):
+        if  amount <= self.balance:
+            print("Balance is sufficient")
+            self.balance -= amount
+        else:
+            print("Insufficient balance")
+
+# Define a display_balance method:
+#   - It should print the current balance
+    def display_balance(self):
+        print(f"Current balance is :{self.balance} ")
+
+withdraw1 = BankAccount(1000)
+#withdraw1.deposit(1000)
+withdraw1.withdraw(500)
+withdraw1.display_balance()
+
+print("-----------------------------------------------------------------------------------------------")
+
+"""22. Write a class Student with methods: 
+● add_marks(marks_list)
+● calculate_average() 
+● is_pass() (average ≥ 40) """
+
+# Create a class named Student
+class Student:
+
+# Initialize the student with an empty list to store marks
+    def __init__(self):
+        self.marks = []
+
+# Define add_marks(marks_list) method:
+#   - It should accept a list of marks
+#   - Add these marks to the student's marks list
+    def add_marks(self,marks_list):
+        if not isinstance(marks_list, list):
+            print("Please provide a list of marks")
+        elif len(marks_list) == 0:
+            print("Marks list is empty")
+        elif isinstance(marks_list, list):
+            self.marks.extend(marks_list)
+        else:
+            self.marks.append(marks_list)
+
+# Define calculate_average() method:
+#   - It should calculate the average of all marks
+#   - Return the average value
+#   - If there are no marks, return 0 or show a message
+    def calculate_average(self):
+       if len(self.marks)==0:
+           return 0
+       else:
+           return sum(self.marks) / len(self.marks)
+
+# Define is_pass() method:
+#   - It should call calculate_average() to get the average
+#   - If average is greater than or equal to 40, return True or print "Pass"
+#   - Otherwise return False or print "Fail"
+    def is_pass(self):
+        average = self.calculate_average()
+        if  average >= 40:
+            print("Pass")
+        else:
+            print("Fail")
+
+student1 = Student()
+student1.add_marks([60, 80])
+print(student1.marks)
+print(student1.calculate_average())
+student1.is_pass()
+
+print("-----------------------------------------------------------------------------------------------")
+
+"""23. Create a class LoginSystem with methods: 
+● register(username, password) 
+● login(username, password) 
+Store credentials inside the object. """
+
+# Create a class LoginSystem
+class LoginSystem:
+
+# Initialize the class with a data structure (like a dictionary)
+# This will store usernames and passwords inside the object
+    def __init__(self):
+        self.credentials = {}
+
+# Define register(username, password) method
+#   - Check if username or password is empty
+#   - Check if the username already exists
+#   - If valid, store username and password in the object
+#   - Show a success or error message
+
+    def register(self, username, password):
+        if username == "" or password == "":
+            print("Username and password cannot be empty")
+
+        if username in self.credentials:
+            print("Username already registered")
+
+        self.credentials[username] = password
+        print("Registration successful")
+
+# Define login(username, password) method
+#   - Check if the username exists in stored credentials
+#   - If not found, show "User not found"
+#   - If found, compare the stored password with the given password
+#   - If password matches, show "Login successful"
+#   - Otherwise, show "Incorrect password"
+
+    def login(self, username, password):
+        if username not in self.credentials:
+            print("User not found")
+
+        if self.credentials[username] != password:
+            print("Incorrect password")
+
+        print("Login successful")
+
+system = LoginSystem()
+system.register("admin", "1234")
+system.login("admin", "1234")
+system.login("admin", "wrong")
+system.register("admin", "newpass")
+
+print("-----------------------------------------------------------------------------------------------")
+"""24. Define a class LibraryBook with methods: 
+● borrow()
+● return_book() 
+● status() 
+Track availability using an instance variable."""
+
+
+# Create a class named LibraryBook
+class LibraryBook:
+# Initialize an instance variable to track availability of the book
+# Set it to True when the book is available
+    def __init__(self):
+        self.available = True
+# Define borrow() method
+#   - Check if the book is available
+#   - If available, mark it as borrowed
+#   - If not available, show a message that the book is already borrowed
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print("Book borrowed successfully")
+        else:
+            # If already borrowed
+            print("Book is already borrowed")
+# Define return_book() method
+#   - Check if the book is currently borrowed
+#   - If borrowed, mark it as available again
+#   - If not borrowed, show a message that the book was not borrowed
+    def return_book(self):
+        if not self.available:
+            self.available = True
+            print("Book returned successfully")
+        else:
+            print("Book was not borrowed")
+# Define status() method
+#   - Check the availability variable
+#   - If available, show "Book is available"
+#   - Otherwise, show "Book is currently borrowed"
+    def status(self):
+        if self.available:
+            print("Book is available")
+        else:
+            print("Book is currently borrowed")
+book1 = LibraryBook()
+book1.status()
+book1.borrow()
+book1.status()
+book1.return_book()
+book1.status()
+
+print("-----------------------------------------------------------------------------------------------")
+
+"""25. Create a class Order with methods: 
+● add_item(price)
+● calculate_total() 
+● apply_discount(percent)"""
+
+# Create a class named Order
+class Order:
+    # Initialize the class with a data structure (like a list)
+    def __init__(self):
+        self.items = []
+        self.total = 0
+
+    # Define add_item(price) method
+    #   - Check if the price is a valid number
+    #   - Ensure the price is greater than 0
+    #   - Add the price to the items list
+    def add_item(self, price=None):
+        if price is None:
+            print("Invalid Input")
+        elif not isinstance(price, (int, float)):
+            print("Price must be a number")
+        elif price <= 0:
+            print("Price must be greater than 0")
+        else:
+            self.items.append(price)
+            print("Item added")
+    #   - Check if there are any items in the order
+    #   - Calculate the sum of all item prices
+    #   - Return or display the total amount
+    def calculate_total(self):
+        if len(self.items) == 0:
+            print("No items in the order")
+            return 0
+        else:
+            self.total = sum(self.items)
+            return self.total
+
+    # Define apply_discount(percent) method
+    #   - Check if the discount percent is valid (between 0 and 100)
+    #   - Calculate the discount based on the current total
+    #   - Reduce the total by the discount amount
+    #   - Display or return the discounted total
+    def apply_discount(self, percent = None):
+        if percent is None:
+            print("Invalid Percentage")
+        if not isinstance(percent, (int, float)):
+            print("Percentage must be a number")
+            return None
+        elif percent < 0 or percent > 100:
+            print("Must be between 0 and 100")
+            return None
+        else:
+            total = self.calculate_total()
+            discount_amount = total * (percent / 100)
+            self.total = total - discount_amount
+            return self.total
+
+
+order1 = Order()
+order1.add_item()
+order1.add_item(80)
+
+print("Total:", order1.calculate_total())
+print("After discount:", order1.apply_discount())
+print("After discount:", order1.apply_discount(20))
